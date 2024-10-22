c2511f366b771a3596bf61f5b2cd8fd3febcb18d
gabinag <gabriela.nagamuta@gmail.com>
2024-10-22 19:45:34 -0300

    cria useFetch

--- dogs/src/Components/Login/LoginCreate.jsx
+++ dogs/src/Components/Login/LoginCreate.jsx
@@ -1,16 +1,19 @@
 import { Input } from  '../Forms/Input';
 import { Button }from '../Forms/Button';
+import { Error }from '../Helper/Error';
 import { useForm } from '../../Hooks/useForm';
 import { USER_POST } from '../../api';
 import { useContext } from 'react';
 import { UserContext } from '../../UserContext';
+import { useFetch } from '../../Hooks/useFetch';
 
 export const LoginCreate = () => {
   const username = useForm();
   const email = useForm('email');
   const password = useForm();
 
-  const {userLogin} = useContext(UserContext);
+  const { userLogin } = useContext(UserContext);
+  const { loading, error, request } = useFetch();
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -19,7 +22,7 @@ export const LoginCreate = () => {
       email: email.value,
       password: password.value,
     })
-    const response = await fetch(url, options);
+    const { response }= await request(url, options);
     if(response.ok) userLogin(username.value, password.value);
   }
 
@@ -30,7 +33,10 @@ export const LoginCreate = () => {
         <Input label="Usuário" type="text" name="username" {...username}/>
         <Input label="Email" type="email" name="email" {...email}/>
         <Input label="Senha" type="password" name="password" {...password}/>
-        <Button>Cadastrar</Button>
+        {loading ? (<Button disabled>Cadastrando...</Button>) : (
+          <Button>Cadastrar</Button>
+        )}
+        <Error error={error}/>
       </form>
     </section>
   )
