376d2a66287baaed3f0faf426372b0125442e573
gabinag <gabriela.nagamuta@gmail.com>
2024-11-14 19:22:12 -0300

    scroll infinito e minhas fotos

--- dogs/src/Components/User/User.jsx
+++ dogs/src/Components/User/User.jsx
@@ -3,13 +3,17 @@ import { Routes, Route } from "react-router-dom";
 import { Feed } from '../Feed/Feed';
 import { UserPhotoPost } from '../User/UserPhotoPost';
 import { UserStats }from '../User/UserStats';
+import { useContext } from "react";
+import { UserContext } from "../../UserContext";
 
 export const User = () => {
+  const { data } = useContext(UserContext);
+
   return (
     <section className="container">
         <UserHeader />
         <Routes>
-            <Route path="/" element={<Feed />}/>
+            <Route path="/" element={<Feed user={data.id}/>}/>
             <Route path="/postar" element={<UserPhotoPost />}/>
             <Route path="/estatisticas" element={<UserStats />}/>
         </Routes>
