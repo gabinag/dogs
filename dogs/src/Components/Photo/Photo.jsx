0faca288cc1db82f097f75a0b569aa55cb9938c2
gabinag <gabriela.nagamuta@gmail.com>
2024-11-28 19:04:26 -0300

    Foto página interna

---
+++ dogs/src/Components/Photo/Photo.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import { PHOTO_GET } from "../../api";
+import { Error } from "../Helper/Error";
+import { Loading } from "../Helper/Loading";
+import { PhotoContent } from "./PhotoContent";
+import { useParams } from "react-router-dom";
+import { useFetch } from '../../Hooks/useFetch';
+
+export const Photo = () => {
+  const {id} = useParams();
+  const {data, loading, error, request} = useFetch();
+
+  useEffect(() => {
+    const {url, options} = PHOTO_GET(id);
+    request(url, options);
+  }, [request, id]);
+
+  if(error) return <Error error={error}/>;
+  if(loading) return <Loading />;
+  if(data)
+    return (
+        <section className="container mainContainer">
+            <PhotoContent single={true} data={data} />
+        </section>
+    )
+    else return null;
+}
