9837ae9a1f3ab84e85360a3adbca087af934aa6d
gabinag <gabriela.nagamuta@gmail.com>
2024-11-28 19:18:01 -0300

    Adiciona página 404

---
+++ dogs/src/Components/NotFound.jsx
@@ -0,0 +1,9 @@
+
+export const NotFound = () => {
+  return (
+    <div className="container mainContainer">
+        <h1 className="title">Error 404</h1>
+        <p>Página não encontrada.</p>
+    </div>
+  )
+}
--- dogs/src/Components/User/User.jsx
+++ dogs/src/Components/User/User.jsx
@@ -5,6 +5,7 @@ import { UserPhotoPost } from '../User/UserPhotoPost';
 import { UserStats }from '../User/UserStats';
 import { useContext } from "react";
 import { UserContext } from "../../UserContext";
+import { NotFound } from "../NotFound";
 
 export const User = () => {
   const { data } = useContext(UserContext);
@@ -16,6 +17,7 @@ export const User = () => {
             <Route path="/" element={<Feed user={data.id}/>}/>
             <Route path="/postar" element={<UserPhotoPost />}/>
             <Route path="/estatisticas" element={<UserStats />}/>
+            <Route path='*' element={<NotFound />} />
         </Routes>
     </section>
   )
