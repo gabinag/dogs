a23b0648206d1564231c0588c62d76c6f2eecca4
gabinag <gabriela.nagamuta@gmail.com>
2024-11-28 20:39:43 -0300

    cria footer

--- dogs/src/Components/Footer.jsx
+++ dogs/src/Components/Footer.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import styles from './Footer.module.css'
+import styles from './Footer.module.css';
+import Dogs from '../Assets/dogs-footer.svg?react';
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <Dogs />
+      <p>Dogs. Alguns direitos reservados.</p>
+    </footer>
   )
 }
